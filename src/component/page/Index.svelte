6b739e2a05e92e61600f3923a53d2aae37b69802
donghoon <rainbow-aaaa@nate.com>
2022-07-24 20:53:04 +0900

    파일명 수정

--- src/component/page/Index.svelte
+++ src/component/page/Index.svelte
@@ -16,36 +16,21 @@
       </div>
       <div class="col-12 col-md-7 col-lg-5 order-md-1" data-aos="fade-up">
         <!-- Heading -->
-        <div class="display-5 text-center text-md-start">
-          어떤 OTT를 볼지 고민하지 말고
-        </div>
-        <div class="display-3 text-center text-md-start mb-5 text-secondary">
-          그냥 전부 보세요!
-        </div>
+        <div class="display-5 text-center text-md-start">어떤 OTT를 볼지 고민하지 말고</div>
+        <div class="display-3 text-center text-md-start mb-5 text-secondary">그냥 전부 보세요!</div>
         <div class="col-12 col-md-5 col-lg-6 order-md-2">
           <!-- Image -->
-          <img
-            src="./assets/img/main/main_img.png"
-            class="main-m img-fluid mw-md-150 mw-lg-100 mb-4 mb-md-0"
-            alt="..."
-            data-aos="fade-up"
-          />
+          <img src="./assets/img/main/main_img.png" class="main-m img-fluid mw-md-150 mw-lg-100 mb-4 mb-md-0" alt="..." data-aos="fade-up" />
         </div>
         <div class="main-box text-center text-md-start">
-          <div
-            class="card rounded-lg shadow-lg mb-6 mb-md-0 bg-primary"
-            style="z-index: 1;"
-            data-aos="fade-up"
-          >
+          <div class="card rounded-lg shadow-lg mb-6 mb-md-0 bg-primary" style="z-index: 1;" data-aos="fade-up">
             <!-- Body -->
             <div class="card-body">
               <div class="row justify-content-center">
                 <div class="col-12 ">
                   <!-- Badge -->
                   <div class="text-center mt-2">
-                    <span class="h5 text-uppercase text-white"
-                      >5개 OTT를 모두 보는데</span
-                    >
+                    <span class="h5 text-uppercase text-white">5개 OTT를 모두 보는데</span>
                   </div>
 
                   <!-- Price -->
@@ -54,16 +39,9 @@
                   </div>
 
                   <!-- Text -->
-                  <p class="text-center text-white mb-5 display-8">
-                    직접 공유하는 파티장 가격
-                  </p>
+                  <p class="text-center text-white mb-5 display-8">직접 공유하는 파티장 가격</p>
 
-                  <button
-                    href="#"
-                    class="fw-bold card-btn btn-lg w-100 btn-secondary fs-4"
-                  >
-                    바로 알아보기
-                  </button>
+                  <button href="#" class="fw-bold card-btn btn-lg w-100 btn-secondary fs-4"> 바로 알아보기 </button>
                 </div>
               </div>
               <!-- / .row -->
@@ -81,16 +59,8 @@
   <div class="container ">
     <div class="row justify-content-center">
       <div class="col-12 col-md-10 col-lg-8 text-center">
-        <div
-          class="d-flex align-items-center justify-content-center mb-6 mb-sm-8"
-        >
-          <span
-            ><img
-              src="./assets/img/emoji/star.png"
-              class="me-1 emoji"
-              alt="..."
-            /></span
-          >
+        <div class="d-flex align-items-center justify-content-center mb-6 mb-sm-8">
+          <span><img src="./assets/img/emoji/star.png" class="me-1 emoji" alt="..." /></span>
           <!-- Heading -->
           <div class="display-5">에브리뷰에서는 이런게 특별해요</div>
         </div>
@@ -98,62 +68,42 @@
     </div>
     <!-- / .row -->
 
-    <div
-      class="d-flex align-items-center justify-content-between mb-8 main-con"
-    >
+    <div class="d-flex align-items-center justify-content-between mb-8 main-con">
       <div class="col-12 col-md-7 order-md-2 card_main" data-aos="fade-left">
         <!-- Heading -->
         <h3>
           내 계정을 공유하는 파티장은<br />
           <span class="display-5 text-primary">이용료를 더 적게 부담해요</span>
         </h3>
-        <p class="text-muted mb-0">
-          파티원을 관리하는 파티장은 이용료 혜택이 있어야죠!
-        </p>
+        <p class="text-muted mb-0">파티원을 관리하는 파티장은 이용료 혜택이 있어야죠!</p>
       </div>
       <div class="col-12 col-md-5 col-lg-3 order-md-1 " data-aos="fade-right">
         <!-- Card -->
         <div class="d-flex align-items-center justify-content-center">
           <!-- Image -->
-          <img
-            src="./assets/img/main/main_1.png"
-            class="img-fluid mb-4 mb-md-0 main_img"
-            alt="..."
-            data-aos="fade-up"
-            data-aos-delay="100"
-          />
+          <img src="./assets/img/main/main_1.png" class="img-fluid mb-4 mb-md-0 main_img" alt="..." data-aos="fade-up" data-aos-delay="100" />
         </div>
       </div>
     </div>
     <!-- / .row -->
-    <div
-      class="d-flex align-items-center justify-content-between mb-8 main-con"
-    >
+    <div class="d-flex align-items-center justify-content-between mb-8 main-con">
       <div class="col-12 col-md-7 col-lg-7 card_main" data-aos="fade-right">
         <!-- Heading -->
         <h3>
           꼭 최고화질일 필요 있나요?<br />
           <span class="display-5 text-primary">원하는 화질로 보세요</span>
         </h3>
-        <p class="text-muted mb-0">
-          스탠다드 화질로 이용하면 당연히 더 저렴해요!
-        </p>
+        <p class="text-muted mb-0">스탠다드 화질로 이용하면 당연히 더 저렴해요!</p>
       </div>
       <div class="col-12 col-md-5 col-lg-3" data-aos="fade-left">
         <div class="d-flex align-items-center justify-content-center">
           <!-- Image -->
-          <img
-            src="./assets/img/main/main_2.png"
-            class="img-fluid mb-4 mb-md-0 main_img"
-            alt="..."
-          />
+          <img src="./assets/img/main/main_2.png" class="img-fluid mb-4 mb-md-0 main_img" alt="..." />
         </div>
       </div>
     </div>
     <!-- / .row -->
-    <div
-      class="d-flex align-items-center justify-content-between mb-8 main-con"
-    >
+    <div class="d-flex align-items-center justify-content-between mb-8 main-con">
       <div class="col-12 col-md-7 order-md-2 card_main" data-aos="fade-left">
         <!-- Heading -->
         <h3>
@@ -161,27 +111,17 @@
           <span class="display-5 text-primary">나머지는 에브리뷰가 해요</span>
         </h3>
 
-        <p class="text-muted mb-0">
-          파티원 모집도, 원하는 파티를 찾는 것도, 에브리뷰가 해드려요!
-        </p>
+        <p class="text-muted mb-0">파티원 모집도, 원하는 파티를 찾는 것도, 에브리뷰가 해드려요!</p>
       </div>
       <div class="col-12 col-md-5 col-lg-3" data-aos="fade-right">
         <div class="d-flex align-items-center justify-content-center">
           <!-- Image -->
-          <img
-            src="./assets/img/main/main_3.png"
-            class="img-fluid mb-4 mb-md-0 main_img"
-            alt="..."
-            data-aos="fade-up"
-            data-aos-delay="100"
-          />
+          <img src="./assets/img/main/main_3.png" class="img-fluid mb-4 mb-md-0 main_img" alt="..." data-aos="fade-up" data-aos-delay="100" />
         </div>
       </div>
     </div>
     <!-- / .row -->
-    <div
-      class="d-flex align-items-center justify-content-between mb-6 main-con"
-    >
+    <div class="d-flex align-items-center justify-content-between mb-6 main-con">
       <div class="col-12 col-md-7 col-lg-7 card_main " data-aos="fade-right">
         <!-- Heading -->
         <h3>
@@ -190,18 +130,12 @@
         </h3>
 
         <!-- Text -->
-        <p class="text-muted mb-0">
-          언제든지 나가세요. 이용하지 않은 기간은 100% 환불을 보장해요!
-        </p>
+        <p class="text-muted mb-0">언제든지 나가세요. 이용하지 않은 기간은 100% 환불을 보장해요!</p>
       </div>
       <div class="col-12 col-md-5 col-lg-3 order-md-1" data-aos="fade-left">
         <div class="d-flex align-items-center justify-content-center">
           <!-- Image -->
-          <img
-            src="./assets/img/main/main_4.png"
-            class="img-fluid mb-4 mb-md-0 main_img"
-            alt="..."
-          />
+          <img src="./assets/img/main/main_4.png" class="img-fluid mb-4 mb-md-0 main_img" alt="..." />
         </div>
       </div>
     </div>
@@ -214,13 +148,7 @@
       <div class="col-12 col-md-5 col-lg-4" data-aos="fade-right">
         <div class="d-flex align-items-center justify-content-center">
           <!-- Image -->
-          <img
-            src="./assets/img/main/share.png"
-            class="img-fluid mb-6 mb-md-0 main_img"
-            alt="..."
-            data-aos="fade-up"
-            data-aos-delay="100"
-          />
+          <img src="./assets/img/main/share.png" class="img-fluid mb-6 mb-md-0 main_img" alt="..." data-aos="fade-up" data-aos-delay="100" />
         </div>
       </div>
       <div class="col-12 col-md-7 main-share" data-aos="fade-left">
@@ -231,8 +159,7 @@
         <div class="d-flex share-btn">
           <button class=" btn btn-gray mt-6 me-lg-4"> url 복사 </button>
           <button class=" btn btn-kakao mt-6 ">
-            <img src="./assets/img/kakao.svg" class="me-2" alt="..." />카카오톡
-            공유
+            <img src="./assets/img/Kakao.svg" class="me-2" alt="..." />카카오톡 공유
           </button>
         </div>
       </div>
